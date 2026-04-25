fa0c2f34ee547ef079ed4f3a1d57b52adb9fca02
Rasel <raselranar2005@gmail.com>
2026-04-25 23:14:44 +0600

    save

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,3 +1,5 @@
+import { Button } from "@heroui/react";
+import { Icon } from "@iconify/react";
 import CategoryList from "@/components/homepage/news/CategoryList";
 import { secondsToMinutes } from "date-fns";
 import Image from "next/image";
@@ -13,11 +15,25 @@ export default async function Home() {
     data: { news_category: categories },
   } = await getCategory();
   return (
-    <section className="container mx-auto grid grid-cols-4 mt-20">
+    <section className="container mx-auto grid-cols-1 grid sm:grid-cols-4 mt-20">
       {/* categories */}
       <CategoryList categories={categories} />
       <div className="col-span-2">Dragon News Home</div>
-      <div className="">Social Icons</div>
+      {/* Social Icons */}
+      <div className="">
+        <h2 className="text-lg mb-2">Social Icons</h2>
+
+        <div className="flex w-full max-w-xs flex-col gap-3">
+          <Button className="w-full" variant="tertiary">
+            <Icon icon="devicon:google" />
+            Login in with Google
+          </Button>
+          <Button className="w-full" variant="tertiary">
+            <Icon icon="mdi:github" />
+            Sign in with GitHub
+          </Button>
+        </div>
+      </div>
     </section>
   );
 }
